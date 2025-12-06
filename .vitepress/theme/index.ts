b136e4db5926d06a2943dfab57bb2f63f4e9ceee
Haggai Wambua <haggai.wambua@gmail.com>
2025-12-06 18:59:45 -0500

    Updated config

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,5 @@
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
+import Theme from 'vitepress/theme'
 import './style/index.css'
 
-export default {
-  extends: DefaultTheme,
-} satisfies Theme
+export default Theme
 
